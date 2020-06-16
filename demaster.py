f381c55339fdbe111c7d4cb7083834115751686c
hankhank10 <m.a.hankinson@gmail.com>
2020-06-16 07:53:37 +0100

    Create demaster.py

---
+++ demaster.py
@@ -0,0 +1,47 @@
+import sys
+# this is the demaster script which removes nonsense like "Remastered" and "Live at" from track names
+# latest can be downloaded from https://github.com/hankhank10/demaster
+
+def strip_name(full_song_name):
+
+    text_to_parse = full_song_name
+    lower_text_to_parse = text_to_parse.lower
+
+    offending_text = [
+        '- Remast',
+        '(Remast',
+        '- Live at',
+        '(Live at',
+        '- Mono / Remast',
+        '- From '
+        ]
+
+    for x in range (1990,2025):
+        new_offending_text = '- ' + str(x) + ' Remast'
+        offending_text.append (new_offending_text)
+
+    for x in range (1990,2025):
+        new_offending_text = '(' + str(x) + ' Remast'
+        offending_text.append (new_offending_text)
+
+    for item in offending_text:
+        if text_to_parse.find(item) >=0:
+            split_out_text = text_to_parse.partition (item)
+            return split_out_text[0]
+
+    return full_song_name
+
+for a in range (1, 100):
+    print("")
+
+while True:
+    name_to_check = input (">> ")
+
+    text_to_print = strip_name(name_to_check)
+    print (text_to_print)
+    print ("")
+    print ("")
+
+    f = open ("unremaster-output.txt", "a")
+    f.write (name_to_check + " >>> " + text_to_print)
+    f.close
