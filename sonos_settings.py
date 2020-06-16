8b34aed80c7fc84cca4005f4b932bef7a49e5051
Hank <m.a.hankinson@gmail.com>
2020-06-16 07:52:09 +0100

    Added integration for demaster

--- sonos_settings.py
+++ sonos_settings.py
@@ -2,3 +2,4 @@
 
 sonos_http_api_address = "localhost"
 sonos_http_api_port = "5005"
+demaster = True #crop song names to remove nonsense such as "Remastered" or "live at"
--- sonos_user_data.py
+++ sonos_user_data.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import sonos_settings
+import demaster
 
 def find_unknown_radio_station_name(filename):
     # BBC streams started via alexa don't return their real name, which is annoying... but fixable:
@@ -70,4 +71,7 @@ def current(sonos_room):
         if 'album' in obj['currentTrack']: current_album = obj['currentTrack']['album']
         if 'absoluteAlbumArtUri' in obj['currentTrack']: current_image = obj['currentTrack']['absoluteAlbumArtUri']
     
+    if sonos_settings.demaster == True:
+        current_trackname = demaster.strip_name (current_trackname)
+
     return current_trackname, current_artist, current_album, current_image, playing_status
