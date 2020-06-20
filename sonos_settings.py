4dfd18be47cd04e0671dcdfb0621f72329648f1a
hankhank10 <m.a.hankinson@gmail.com>
2020-06-20 14:31:35 +0100

    Update sonos_settings.py

--- sonos_settings.py
+++ sonos_settings.py
@@ -2,7 +2,7 @@
 
 ## General settings
 
-sonos_http_api_address = "192.168.4.102"
+sonos_http_api_address = "localhost"
 sonos_http_api_port = "5005"
 
 demaster = True #crop song names to remove nonsense such as "Remastered" or "live at"
