3feb48feae0ec0f591db2667ba5500438505fba2
ashenshugarRET <42817877+ashenshugarRET@users.noreply.github.com>
2022-02-12 11:30:10 +0000

    Update go_sonos_highres.py to allow text to overlay full screen album art
    Adding the ability to overlay track title, Album, and Artist details over a fullscreen album art image. Requires a new parameter (overlay_text) to be set to True in sonos_settings.py, if set to False text is displayed below a thumbnail of the album art

--- go_sonos_highres.py
+++ go_sonos_highres.py
@@ -154,8 +154,9 @@ async def main(loop):
     setup_logging()
     log_git_hash()
     show_details_timeout = getattr(sonos_settings, "show_details_timeout", None)
+    
     try:
-        display = DisplayController(loop, sonos_settings.show_details, sonos_settings.show_artist_and_album, show_details_timeout)
+        display = DisplayController(loop, sonos_settings.show_details, sonos_settings.show_artist_and_album, show_details_timeout, sonos_settings.overlay_text)
     except SonosDisplaySetupError:
         loop.stop()
         return
