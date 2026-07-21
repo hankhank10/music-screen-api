a3ac6d43eb14ed8c59b4125b973e9c2d5d2656ca
fazalpsinfo-cmyk <fazalpsinfo@gmail.com>
2026-07-21 18:13:23 +0530

    Fix typo: 'sytem' -> 'system'

--- go_sonos.py
+++ go_sonos.py
@@ -1,5 +1,5 @@
 # This file is for use with the Pimoroni inky wHAT display
-# it integrates with your local Sonos sytem to display what is currently playing
+# it integrates with your local Sonos system to display what is currently playing
 
 import time # needed to delay x seconds between checks
 import sonos_user_data_legacy as sonos_user_data # the api which pulls the lastfm data
